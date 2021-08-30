7275f4662235293ffaabc4d083a3b853c082f61a
DeltaProxima <vadeshpande2002@gmail.com>
2021-08-30 18:58:16 +0530

    Issue Solved: Image not Showing up

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -119,12 +119,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/static')
+    os.path.join(BASE_DIR, 'static/ecommerce')
 ]
 
-MEDIA_URL = 'static/images/'
+MEDIA_URL = '/images/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media/images')
